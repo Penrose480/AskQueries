03e639c4682cc0c4936dc2a11dc29350453b92de
Penrose480 <Penrose480@proton.me>
2026-07-02 10:06:40 -0700

    removed unnecessray bloat

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from transformers import pipeline
 inp = input()  
 
 ans = pipeline("question-answering")
-summarizer = pipeline("summarization")
 
 context = input("Enter the doc for context: ")
-print("Here is a quick summmary: ", summarizer(context, max_length=len(context)//3))
 
 while (True):
     question = input("Ask a question about the doc (q to quit): ")
